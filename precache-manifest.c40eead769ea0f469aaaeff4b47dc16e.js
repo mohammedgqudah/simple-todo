2194818353c3634d38b4ba2ad21c9e9bab27eac2
mohammedgqudah <mohghq@gmail.com>
2020-02-08 12:58:39 +0200

    Updates

---
+++ precache-manifest.c40eead769ea0f469aaaeff4b47dc16e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8881efecc11b46b3359ab1529af1bca0",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "fe55c2a95611b60af810",
+    "url": "/simple-todo/static/css/main.aca03d47.chunk.css"
+  },
+  {
+    "revision": "3cdc06e64f38a5babfb1",
+    "url": "/simple-todo/static/js/2.e0beccdb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-todo/static/js/2.e0beccdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe55c2a95611b60af810",
+    "url": "/simple-todo/static/js/main.eb5e47d1.chunk.js"
+  },
+  {
+    "revision": "12880873365c35826dad",
+    "url": "/simple-todo/static/js/runtime-main.31c5c200.js"
+  },
+  {
+    "revision": "2084a0546690bd800abf5123ae724799",
+    "url": "/simple-todo/static/media/discord_checkmark-transparent.2084a054.png"
+  }
+]);
